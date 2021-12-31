efb2cef825c20cfddedc6706bd50d4e3d175676d
IHH123 <izzudin.hanan3@gmail.com>
2021-12-31 07:13:29 +0700

    Memberikan method animate scroll pada navigasi breakpoint extra small (1)

--- js/Home.js
+++ js/Home.js
@@ -911,7 +911,7 @@ function NAVBAR_BUTTON_SM (e) {
 
             $(document).ready(function(){
 
-                $("html").scrollTop(970);
+                // $("html").scrollTop(970);
                 // $("html").animate({scrollTop: 970},1000,"jswing");
                 $("html").animate({scrollTop: 970},1000);
             });                        
@@ -922,7 +922,7 @@ function NAVBAR_BUTTON_SM (e) {
 
                 $(document).ready(function(){
 
-                    $("html").scrollTop(1970);
+                    // $("html").scrollTop(1970);
                     // $("html").animate({scrollTop: 1970},1000,"jswing");
                     $("html").animate({scrollTop: 1970},1000);
                 });
